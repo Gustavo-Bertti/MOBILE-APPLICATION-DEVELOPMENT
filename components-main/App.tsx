4ba32389bdb4af0eef963b6234db584e7a7536eb
Gustavo Bertti <88666639+Gustavo-Bertti@users.noreply.github.com>
2024-03-14 08:55:58 -0300

    Add files via upload

---
+++ components-main/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import ViewBoxesWithColor from './Componentes/MyView';
+import MyText from './Componentes/MyText';
+import MyImage from './Componentes/MyImage';
+import MyTextInput from './Componentes/MyTextInput';
+import MyScrollView from './Componentes/MyScrollView';
+import MyStyle from './Componentes/MyStyle';
+import MyButton from './Componentes/MyButton';
+import MyList from './Componentes/MyFlatList';
+import MySectionList from './Componentes/MySectionList';
+import MyActivityIndicator from './Componentes/MyActivityIndicator';
+import MyAlert from './Componentes/MyAlert';
+import MyAnimated from './Componentes/MyAnimated';
+import MyLinking from './Componentes/MyLinking';
+import MyModal from './Componentes/MyModal';
+import MyPixelRatio from './Componentes/MyPixelRatio';
+import MyRefreshControl from './Componentes/MyRefreshControl';
+import MyStatusBar from './Componentes/MyStatusBar';
+
+function App(): React.JSX.Element {
+  return <MyStatusBar />;
+}
+
+export default App;
